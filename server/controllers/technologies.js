d2b60b405057f39394b2200c17076113a537f571
Adam Wilbert <adam.wilbert@gmail.com>
2016-09-14 15:13:43 -0700

    add backend cat search

--- server/controllers/technologies.js
+++ server/controllers/technologies.js
@@ -4,11 +4,13 @@ const Technology = require("../models/technology")
 module.exports = {
 
     fewd(req, res) {
-        Technology.find({ "category": "fewd" }, function (err, technologies) {
+        Technology.find({ "category": "fewd" },  (err, technologies) => {
             res.json(technologies)
         })
     },
     bewd(req, res) {
-        res.json("bewd is bae")
+        Technology.find({ "category": "bewd" },  (err, technologies) => {
+            res.json(technologies)
+        })
     }
 }
